487cac0d2518d721db1d31274dc7335e0c7a093c
andreadisaro <andrea.disaro@aevoluta.com>
2023-06-03 21:14:44 +0200

    items api

--- backend/src/main/java/it/giochisulnostrotavolo/listone/controller/ApiController.java
+++ backend/src/main/java/it/giochisulnostrotavolo/listone/controller/ApiController.java
@@ -1,5 +1,8 @@
 package it.giochisulnostrotavolo.listone.controller;
 
+import it.giochisulnostrotavolo.listone.entities.Item;
+import it.giochisulnostrotavolo.listone.service.ItemService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @RequestMapping("/public/api")
 public class ApiController {
 
@@ -24,32 +28,15 @@ public class ApiController {
   );
 
   @Autowired
-  private RestTemplate restTemplate;
+  private ItemService itemService;
 
   @GetMapping("heartBeat")
   public ResponseEntity<String> heartBeat() {
     return ResponseEntity.ok().body("OK");
   }
 
-  @PostMapping("{path}")
-  public ResponseEntity<String> postExample(
-    @RequestBody Object request,
-    @PathVariable("path") String path
-  ) {
-    LOG.info(
-      "\n\nChiamata a ".concat(path)
-        .concat("\nInput:\n")
-        .concat(request.toString())
-    );
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    HttpEntity<Object> entity = new HttpEntity<>(request, headers);
-    ResponseEntity<String> response = restTemplate.postForEntity(
-      "/" + path,
-      entity,
-      String.class
-    );
-    LOG.info("\nOutput".concat(response.getBody()));
-    return response;
+  @GetMapping("items")
+  public List<Item> items() {
+    return itemService.findAll();
   }
 }
