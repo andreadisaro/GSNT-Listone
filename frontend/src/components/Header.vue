1379d3d1f94b214cf14814a78fc3702c73f0685f
andreadisaro <107838205+andreadisaro@users.noreply.github.com>
2024-10-25 22:23:42 +0200

    lucca24

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,26 +5,21 @@ import SvgIcon from "./SvgIcon.vue";
 </script>
 
 <template>
-  <div
-    class="w-full items-center px-4 flex flex-row justify-between align-middle header py-1 z-40"
-  >
+  <div class="w-full items-center px-4 flex flex-row justify-between align-middle header py-1 z-40">
     <!--SvgIcon
       :name="dataStore.eventLogo"
       class="h-12 w-12 my-auto"
       v-if="dataStore.eventLogo"
     /-->
-    <img src="../assets/play.svg" class="h-12 w-12 my-auto" />
+    <img src="../assets/lucca24.png" class="h-12 w-12 my-auto" />
     <!--div class="text-[23px] font-bold w-full flex-1 text-center">
       {{ dataStore.eventTitle }}
     </div-->
     <div class="text-[23px] font-bold w-full flex-1 text-center">
-      PLAY 2024
+      LUCCA C&G 2024
     </div>
-    <SvgIcon
-      name="Hamburger_icon"
-      class="h-6 w-6 my-auto fill-white"
-      @click="dataStore.showMenu = !dataStore.showMenu"
-    />
+    <SvgIcon name="Hamburger_icon" class="h-6 w-6 my-auto fill-white"
+      @click="dataStore.showMenu = !dataStore.showMenu" />
   </div>
 </template>
 
